471f1ba67ca6b1b2627db94faf5704b4b0391502
douglas <dougamanzelmann@gmail.com>
2012-08-01 12:03:23 -0400

    end of chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,16 +32,16 @@ describe "User Pages" do
 			end
 		end
 
-		describe "with valid information" do 
-			before do 
-				fill_in "Name", 		with: "Example User"
-				fill_in "Email",		with: "user@example.com" 
-				fill_in "Password", 	with: "foobar"
-				fill_in "Confirmation", with: "foobar"
-			end
+		describe "with valid information" do
+      		before do
+        		fill_in "Name",         with: "Example User"
+        		fill_in "Email",        with: "user@example.com"
+        		fill_in "Password",     with: "foobar"
+        		fill_in "Password confirmation", with: "foobar"
+      		end
 
-			it "should create a user" do 
-				expect { click_button submit }.to change(User, :count).by(1)
+      		it "should create a user" do
+        		expect { click_button submit }.to change(User, :count).by(1)
 			end
 		end
 	end
